93e041fc313dbfb3e836653559b6abdef307f551
István Miklós <miklos.istvan2@gmail.com>
2017-03-30 19:07:42 +0200

    login + logout
    s2ui install with dependencies, create the login form.

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -120,7 +120,9 @@ log4j.main = {
            'net.sf.ehcache.hibernate'
 }
 
-
+grails.plugin.springsecurity.logout.postOnly = true
+grails.plugin.springsecurity.logout.afterLogoutUrl = "/"
+grails.plugin.springsecurity.successHandler.defaultTargetUrl = '/'
 // Added by the Spring Security Core plugin:
 grails.plugin.springsecurity.userLookup.userDomainClassName = 'tube.User'
 grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'tube.UserAuthority'
@@ -128,17 +130,18 @@ grails.plugin.springsecurity.authority.className = 'tube.Authority'
 grails.plugin.springsecurity.controllerAnnotations.staticRules = [
 	'/':                ['permitAll'],
 	'/index':           ['permitAll'],
-	'/index.gsp':       ['permitAll'],
 	'/assets/**':       ['permitAll'],
 	'/**/js/**':        ['permitAll'],
 	'/**/css/**':       ['permitAll'],
 	'/**/images/**':    ['permitAll'],
+	'/register/*':      ['permitAll'],
 	'/**/favicon.ico':  ['permitAll']
 ]
 
 grails.plugin.springsecurity.interceptUrlMap = [
 	'/login/**':		['IS_AUTHENTICATED_ANONYMOUSLY'],
 	'/logout/**':		['IS_AUTHENTICATED_ANONYMOUSLY'],
-	'/comment/**':		['permitAll']
+	'/comment/**':		['permitAll'],
+	'/index.gsp':       ['permitAll']
 ]
 
